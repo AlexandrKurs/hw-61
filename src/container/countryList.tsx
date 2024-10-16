eb75e6864ede949de06cb8bf0fd143bf93a35ea1
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-10-17 00:32:59 +0600

    Made a clickable list of countries. Added a country data block.

--- src/container/countryList.tsx
+++ src/container/countryList.tsx
@@ -4,17 +4,22 @@ import './countryList.css';
 interface Country {
   alpha3Code: string;
   name: string;
+  flag: string;
+  capital: string;
+  population: number;
+  borders: string[];
 }
 
 const CountryList: React.FC = () => {
   const [countries, setCountries] = useState<Country[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const response = await fetch('https://restcountries.com/v2/all?fields=alpha3Code,name');
+        const response = await fetch('https://restcountries.com/v2/all?fields=alpha3Code,name,flag,capital,population,borders');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
@@ -34,6 +39,10 @@ const CountryList: React.FC = () => {
     fetchCountries();
   }, []);
 
+  const handleCountryClick = (name: string) => {
+    setSelectedCountry(name);
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -49,11 +58,15 @@ const CountryList: React.FC = () => {
           <h3 className="header">Список стран</h3>
           <ul>
             {countries.map(country => (
-              <li key={country.alpha3Code}>
+              <li key={country.alpha3Code} onClick={() => handleCountryClick(country.name)}>
                 {country.name}
               </li>
             ))}
           </ul>
+        </div>
+        <div className="col-6">
+          <h3 className="pt-2">Данные о стране</h3>
+          {selectedCountry ? <p>Вы выбрали: <b>{selectedCountry}</b></p> : <p>Пожалуйста, выберите страну.</p>}
         </div>
       </div>
     </div>
